0024081ba38feaeef55800a41730456a91f77b61
Claudia Agostinelli <cagostinelli@unsa.edu.pe>
2024-10-03 23:43:05 -0500

    se intentó realizar el ejercicio 1 usando join, hubo error de sintaxis por el tipo de argumento que se le manda a la función

--- ejemplo.c
+++ ejemplo.c
@@ -2,8 +2,12 @@
 #include "figures.h"
 
 void display(){
-  char** gwhiteSquare = repeatH((whiteSquare),7);
+/*  char** gwhiteSquare = repeatH((whiteSquare),7);
   char** gknight=reverse( knight);
   char** s=superImpose(gknight,gwhiteSquare);
-  interpreter(s);
+  interpreter(s);*/
+    char** gwhiteSquare = repeatH(join((whiteSquare,reverse(whiteSquare),4));
+    interpreter(gwhiteSquare);
+    
+
 }
