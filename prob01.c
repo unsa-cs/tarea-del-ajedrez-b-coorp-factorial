331018f3290379cb861a1e36e9fd3b224e0efb87
Claudia Agostinelli <cagostinelli@unsa.edu.pe>
2024-10-04 21:10:12 -0500

    se separó la función del problema 1 en variables para la mejor comprensión del código

--- prob01.c
+++ prob01.c
@@ -1,6 +1,8 @@
 #include "chess.h"
 #include "figures.h"
 void display(){
-    char** gwhiteSquare = repeatH(join(whiteSquare,reverse(whiteSquare)),4);
-    interpreter(gwhiteSquare);
-  }
+    char** blackSquare = reverse(whiteSquare);
+    char** pairSquare = join(whiteSquare,blackSquare); 
+    char** lineSquare = repeatH(pairSquare,4);
+    interpreter(lineSquare);
+}
