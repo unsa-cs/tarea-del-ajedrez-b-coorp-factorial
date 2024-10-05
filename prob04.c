4c739ddc51575ca4b760c55363c88a7c23debd78
saph1r0 <sophiqwe123@gmail.com>
2024-10-04 23:47:37 -0500

    del arreglo anteriormente creado se imprimieron todas las whitefigures deseadas, solo faltaría la susperposición

--- prob04.c
+++ prob04.c
@@ -10,5 +10,14 @@ void display(){
     char** table1=repeatH(table,4);
 
     char** whiteFigures[]={rook, knight, bishop,queen, king, bishop, knight, rook };
-    interpreter( whiteFigures[0]);
+    char** fichas=whiteFigures[0];
+
+    for(int i = 1; i<8; i++) {
+        fichas= join(fichas,whiteFigures[i]);
+       /* char** table=join(whiteSquare,blackSquare);
+        char** table1=repeatH(table,4);*/
+ 
+    }
+
+    interpreter(fichas);
 }
