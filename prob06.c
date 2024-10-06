1de4d53607520aa64304ec848ba8e37a8c1a6079
Claudia Agostinelli <cagostinelli@unsa.edu.pe>
2024-10-05 22:27:34 -0500

    se visualiza el tablero completo de ajedrez con sus respectivas fichas

--- prob06.c
+++ prob06.c
@@ -4,30 +4,34 @@
 char** createTablero (int fil, int col){
 
     char** blackSquare = reverse(whiteSquare);
-    char** bpatron=join(blackSquare, whiteSquare);
-    char** wpatron=join(whiteSquare, blackSquare);
+    char** bpatron = join(blackSquare, whiteSquare);
+    char** wpatron = join(whiteSquare, blackSquare);
     
-    char** fourpatron=up(bpatron,wpatron);
-    char** tablero=repeatH(repeatV(fourpatron,fil),col);
+    char** fourpatron = up(bpatron,wpatron);
+    char** tablero = repeatH(repeatV(fourpatron,fil),col);
     return tablero;
 }
 void display(){
     char** upperTable = createTablero(1,4);
     char** middleTable = createTablero(2,4);
-    char** lowerTable = createTablero(1,4);
+    char** downTable = createTablero(1,4);
 
-    char** figures[]={rook, knight, bishop, queen, king, bishop, knight, rook};
-    char** fichas=figures[0];
+    char** figures[] = {rook, knight, bishop, queen, king, bishop, knight, rook};
+    char** fichas = figures[0];
     
-    for(int i = 1; i<8; i++) {
-        fichas= join(fichas,figures[i]);
+    for(int i = 1; i < 8; i++) {
+        fichas = join(fichas,figures[i]);
     }
-    char** gpawn= repeatH(pawn,8);
-    char** upfichas=up(fichas,gpawn);
-    char** downfichas=reverse(up(gpawn,fichas));
+    char** gpawn = repeatH(pawn,8);
+    char** upfichas = up(fichas,gpawn);
+    char** downfichas = reverse(up(gpawn,fichas));
 
-    char** wtableroFichas= superImpose(upfichas,upperTable);
-    char** table=up(wtableroFichas, middleTable);
-    interpreter(downfichas);
+    char** wtableroFichas = superImpose(upfichas,upperTable);
+    char** btableroFichas = superImpose(downfichas,downTable);
+
+    char** middleupTable = up(wtableroFichas, middleTable);
+    char** table = up(middleupTable,btableroFichas);
+
+    interpreter(table);
 }
 
