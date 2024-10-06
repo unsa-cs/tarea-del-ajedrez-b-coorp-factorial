55e0d3e928454f0356e8199201722813a38c79cc
saph1r0 <sophiqwe123@gmail.com>
2024-10-06 01:05:36 -0500

    se intentó crear un tablero 2x2 simétrico, sin embargo se visualizó que las filas eran de un solo color cada una, y que los caballos aparecieron de distinto colores pero no opuestos

--- prob07.c
+++ prob07.c
@@ -13,57 +13,19 @@ char** createTablero (int fil, int col){
     return tablero;
 }
 
-char** create(int fi,int co){
-    char** blackSquare = reverse(whiteSquare);
-    char** **tab;
-    tab=(char** **) malloc(sizeof(char***)*fi);
-    for(int i=0;i<fi;i++){
-        for(int j=0;j<co;j++){
-            tab[i]=(char***)malloc(sizeof(char**)*co);
-            if(i%2==0){
-                if(j%2==0){
-                    tab[i][j]=blackSquare;
-                }
-                else{
-                    tab[i][j]=whiteSquare;
-                }
-            }
-            else{
-                if(j%2==0){
-                    tab[i][j]=whiteSquare;
-                }
-                else{
-                    tab[i][j]=blackSquare;
-                }
-            }
-        }
-    }
-    return **tab;
-}
 
 void display(){
-    char** upperTable = createTablero(1,4);
-    char** middleTable= create(2,4);
-    char** downTable = createTablero(1,4);
-/*
-    char** figures[] = {rook, knight, bishop, queen, king, bishop, knight, rook};
-    char** fichas = figures[0];
-    
-    for(int i = 1; i < 8; i++) {
-        fichas = join(fichas,figures[i]);
-    }
-    char** gpawn = repeatH(pawn,8);
-    char** upfichas = up(fichas,gpawn);
-    char** downfichas = reverse(up(gpawn,fichas));
+    char** upperTable = createTablero(2,2);
+    char** downTable = createTablero(2,2);
+     
+    char** blacknight = reverse(knight);
+    char** wknights = repeatH(rotateL(knight),2); 
+    char** bknights = repeatH(rotateR(blacknight),2);
    
-    char** wtableroFichas = superImpose(upfichas,upperTable);
-    char** btableroFichas = superImpose(downfichas,downTable);
-    
-    char** middleupTable = up(wtableroFichas, middleTable);
-    char** table = up(middleupTable,btableroFichas);
-    
-    char** voltear = rotateL(table);
-*/
-    interpreter(middleTable);
-    free(middleTable);
+    char** wtableroFichas = superImpose(wknights,upperTable);
+    char** btableroFichas = superImpose(bknights,downTable);
+
+    char** table = up(wtableroFichas,btableroFichas);
+
+    interpreter(table);
 }
